976a8a0bf26832a970f198641165ff1e6e9fb3a5
repickym <repickym@gmail.com>
2019-01-31 15:11:45 +0000

    Account and Bank done

---
+++ Assaignment2/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assaignment2
+{
+    class Account : IEquatable<Account>
+    {
+        private string name;
+        private float balance;
+        private static int accNumber = 0;
+
+        public Account(string name)
+        {
+            this.name = name;
+            balance = 0;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public float Balance
+        {
+            get => balance;
+            set => balance = value;
+        }
+
+        public bool Equals(Account other)
+        {
+            return (name.Equals(other.Name) && balance.Equals(other.Balance));
+        }
+
+        public bool Equals(string name)
+        {
+            return name.Equals(Name);
+        }
+    }
+}
---
+++ Assaignment2/Bank.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assaignment2
+{
+    class Bank
+    {
+        private List<Account> accounts;
+
+
+        public Bank()
+        {
+            accounts = new List<Account>();
+        }
+
+        public Account findAccount(string name)
+        {
+            Account mask = new Account(name);
+            return accounts.Find(acc => acc.Equals(name));
+        }
+
+        public Boolean updateBalance(string name, ref float transaction, out string error)
+        {
+            Account acc = findAccount(name);
+            if (acc == null)
+            {
+                error = "Account not found!!!";
+                return false;
+            }
+            else
+            {
+                acc.Balance = acc.Balance + transaction;
+                transaction = 0;
+                error = "";
+                return true;
+            }
+        }
+
+        public Boolean updateName(ref string name, string newName, out string error)
+        {
+            Account acc = findAccount(name);
+            if (acc == null)
+            {
+                error = "Account not found!!!";
+                return false;
+            }
+            else
+            {
+                acc.Name = newName;
+                error = "";
+                return true;
+            }
+        }
+
+        public Boolean createAccount(string name)
+        {
+            try
+            {
+                accounts.Add(new Account(name));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public string accDetails(string name)
+        {
+            Account acc = findAccount(name);
+            return (string.Format("Account name: {0} \n Balance: {1}", acc.Name, acc.Balance));
+        }
+
+    }
+}
